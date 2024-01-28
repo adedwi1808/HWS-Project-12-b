10e2de0de57444161d1fb3e8b504ac37a86a01f3
Ade Dwi Prayitno <adedwip1808@email.com>
2024-01-28 10:28:30 +0700

    FINAL

---
+++ HWS-Project-10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  HWS-Project-10
+//
+//  Created by Ade Dwi Prayitno on 28/01/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ HWS-Project-10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  HWS-Project-10
+//
+//  Created by Ade Dwi Prayitno on 15/01/24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
